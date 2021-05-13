a5846aa605a6d2c69657054f1d786bb81eb40b4f
sheetal303 <sheetalkmr.1001@gmail.com>
2021-05-13 16:25:48 +0530

    nc

--- testing/models.py
+++ testing/models.py
@@ -1,4 +1,14 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
-# 401
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    def get_absolute_url(self):
+        return reverse("testing:post_detail", args=[self.slug])
+
+    def __str__(self):
+        return self.title
--- testing/tests.py
+++ testing/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Post
 
 # Create your tests here.
+
+
+class Model_test(TestCase):
+    def setUp(self):
+        self.blog = Post.objects.create(title="flutter", author="gangu")
+
+    def test_post_model(self):
+        d = self.blog
+        self.assertEqual(str(d), "flutter")
+        self.assertTrue(isinstance(d, Post))
